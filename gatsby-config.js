b997261ff1901f10a4b9c6b2a1d6db3c9d385026
evan <evan.leigh@outlook.com>
2021-05-28 12:41:19 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Food Service Concept / Mockup`,
+    description: `This project is an exercise in making a website that relates to the food industry.`,
+    author: `Evan Smith`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,12 +25,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/layout/footer.js
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const Footer = () => (
+  <footer>
+    <h1>Footer</h1>
+  </footer>
+)
+
+export default Footer
