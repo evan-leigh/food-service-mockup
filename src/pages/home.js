901c9ccb68f80e9a360cf98ca5042442bc53f85f
evan <evan.leigh@outlook.com>
2021-05-28 15:46:08 -0400

    changed index to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,11 +2,11 @@ import * as React from "react"
 import Layout from "../layout/layout"
 import Seo from "../layout/seo"
 
-const IndexPage = () => (
+const Home = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hello World</h1>
+      <h1>Hello World</h1>
   </Layout>
 )
 
-export default IndexPage
+export default Home
