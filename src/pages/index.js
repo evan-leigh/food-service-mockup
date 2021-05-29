888d44b72156c42df90e19480a9ae1680743c7e3
evan <evan.leigh@outlook.com>
2021-05-29 11:15:34 -0400

    displays unfinished header

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,6 @@ import Seo from "../layout/seo"
 const Home = () => (
   <Layout>
     <Seo title="Home" />
-
-      <h1 className="header large"> Heading Large </h1>
-
-      <h2 className="header medium"> Heading Medium </h2>
-      
-      <h3 className="header small"> Heading Small </h3>
-      
-      <p className="paragraph large"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exrcitationem accusamus sed voluptatibus obcaecati aliquam quaerat, iste perspiciati dicta voluptates excepturi dolorem non fugiat provident, nostrum nemo nisi, inventore mollitia illum explicabo eaque sequi. Vitae quis voluptatum, veritatis vero et voluptate?</p>
-      
-      <p className="paragraph medium"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exrcitationem accusamus sed voluptatibus obcaecati aliquam quaerat, iste perspiciati dicta voluptates excepturi dolorem non fugiat provident, nostrum nemo nisi, inventore mollitia illum explicabo eaque sequi. Vitae quis voluptatum, veritatis vero et voluptate?</p>
-
-      <p className="paragraph small"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exrcitationem accusamus sed voluptatibus obcaecati aliquam quaerat, iste perspiciati dicta voluptates excepturi dolorem non fugiat provident, nostrum nemo nisi, inventore mollitia illum explicabo eaque sequi. Vitae quis voluptatum, veritatis vero et voluptate?</p>
-
-      <a href="/" className="link large">Link Large</a>
-      
-      <a href="/" className="link medium">Link Medium</a>
-
-      <a href="/" className="link small">Link Small</a>
   </Layout>
 )
 
