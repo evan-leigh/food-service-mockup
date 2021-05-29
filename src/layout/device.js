74f0656f50aebca4a2656315c9d3d851861b3dab
evan <evan.leigh@outlook.com>
2021-05-29 11:13:15 -0400

    added devices media queries

---
+++ src/layout/device.js
@@ -0,0 +1,11 @@
+const size = {
+    mobile: '640px',
+    tablet: '890px',
+    desktop: '1260px'
+  }
+
+  export const device = {
+    mobile: `(max-width: ${size.mobile})`,
+    tablet: `(max-width: ${size.tablet})`,
+    desktop: `(max-width: ${size.desktop})`,
+  };
