1db608b3d3e292c53ec395b53ddd69558be1432b
evan <evan.leigh@outlook.com>
2021-06-02 18:45:00 -0400

    fixed fonts not rendering correctly

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,19 +4,36 @@ const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.8,
   
-  headerFontFamily: [
-    "Epilogue",
-    "sans-serif",
-  ],
+  headerFontFamily: ["Epilogue", "sans-serif"],
   bodyFontFamily: ["Poppins", "sans-serif"],
+  headerWeight: "900",
 
   overrideStyles: ({adjustFontSizeTo}, option, styles) => ({
     h1: {
-      ...adjustFontSizeTo('45px')
+      ...adjustFontSizeTo('45px'),
     },
     bodyFontFamily: {
       baseLineHeight: 2.4
     }
-  })
+  }),
+  
+  googleFonts: [
+    {
+      name: 'Epilogue',
+      styles: [
+        '900',
+      ],
+    },
+    {
+      name: 'Poppins',
+      styles: [
+        '400',
+        '500',
+        '600',
+        '700',
+      ],
+    },
+  ],
+
 })
 export default typography
